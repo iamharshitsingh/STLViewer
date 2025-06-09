26b31fe5a669ece638818e732aa0d8141a6bb814
HarshitSingh <harshit.singh@cctech.co.in>
2025-06-09 19:53:54 +0530

    Add project files.

---
+++ STLViewer/STLViewer.cpp
@@ -0,0 +1,35 @@
+﻿// STLViewer.cpp : Defines the entry point for the application.
+//
+
+#include "STLViewer.h"
+
+using namespace std;
+
+int main()
+{
+    if (!glfwInit()) return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "STL Viewer", nullptr, nullptr);
+    if (!window) { glfwTerminate(); return -1; }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD\n";
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
---
+++ STLViewer/STLViewer.h
@@ -0,0 +1,9 @@
+﻿// STLViewer.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
